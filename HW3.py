a0efd54ffdac422f4a8a1263d539e468b9ba25d9
Jacob Alexander <ja13029@student.wpsok.org>
2026-08-27 13:38:15 -0500

    added HW3

---
+++ HW3.py
@@ -0,0 +1,28 @@
+#Name: Jacob Alexander
+#Class: 6th Hour
+#Assignment: HW3
+
+
+#1. Print "Hello World!"
+print("Hello World")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+Meaning_of_life = 42
+ThisIsAString = "Yes i agree, this is indeed a string"
+AmILying = True
+#3. Print all three variables on the same print function (at the same time).
+print(Meaning_of_life, ThisIsAString, AmILying)
+#4. Create a variable that asks the user to input an integer.
+RandomVariable = int(input("give me a number"))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+Addition = Meaning_of_life + RandomVariable
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(Addition / 2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+AmILying = False
+#8. Print the value of the boolean variable.
+print(AmILying)
+#9. Create a variable with a number that contains decimals.
+FloatingVariable = 6.9
+#10. Round the number from #9 up or down using the round function.
+FloatingVariable = round(FloatingVariable)
+print(FloatingVariable)
